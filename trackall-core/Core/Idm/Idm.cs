093e0477935093b2197dbf38172a218d8b0bb436
ve <ve@vemic.net>
2020-05-06 22:13:05 +0900

    add Idm model

---
+++ trackall-core/Core/Idm/Idm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vemic.Trackall.AzureFunctions.Core.Idm
+{
+    class Idm
+    {
+        /// <summary>
+        /// ユーザを識別するIDです。
+        /// </summary>
+        public string UserID { get; set; }
+
+        /// <summary>
+        /// IDMを識別するIDです。
+        /// </summary>
+        public string IdmID { get; set; }
+
+        /// <summary>
+        /// IDMのタイトルです。
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// IDMの説明です。
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 予定の開始日です。
+        /// </summary>
+        public DateTime ScheduledStartDateTime { get; set; }
+
+        /// <summary>
+        /// 予定の終了日です。
+        /// </summary>
+        public DateTime ScheduledEndDateTime { get; set; }
+
+        /// <summary>
+        /// 実績の開始日です。
+        /// </summary>
+        public DateTime StartDateTime { get; set; }
+
+        /// <summary>
+        /// 実績の終了日です。
+        /// </summary>
+        public DateTime EndDateTime { get; set; }
+
+        /// <summary>
+        /// ヘッダ情報に含まれない詳細情報です。
+        /// </summary>
+        public object DetailInfo { get; set; }
+    }
+}
