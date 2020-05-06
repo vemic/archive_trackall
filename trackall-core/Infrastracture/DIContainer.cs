19495edbb89ade7afee320a597ae0ce0b3fe4802
ve <ve@vemic.net>
2020-05-06 15:40:14 +0900

    add init DIContainer

---
+++ trackall-core/Infrastracture/DIContainer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Vemic.Trackall.AzureFunctions.Infrastracture
+{
+    public sealed class DIContainer
+    {
+        private static readonly IServiceProvider _instance = Build();
+        public static IServiceProvider Instance => _instance;
+
+        static DIContainer() { }
+        private DIContainer() { }
+
+
+        private static IServiceProvider Build()
+        {
+            var services = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // add services.
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
